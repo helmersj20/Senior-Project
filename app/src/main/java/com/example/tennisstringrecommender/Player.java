fd54f764b1885f3796a6dca04558d8726f175847
Jonathan Helmers <helmersj20@hanover.edu>
2020-02-03 10:20:25 -0500

    Implemented Power level class and updated recommend function inside Player class

--- app/src/main/java/com/example/tennisstringrecommender/Player.java
+++ app/src/main/java/com/example/tennisstringrecommender/Player.java
@@ -7,23 +7,27 @@ public class Player implements Parcelable {
     public String experienceLevel;
     public String armInjuryHistory;
     public String priceLevel;
+    public String powerLevel;
 
     public Player(){
         this.experienceLevel = "";
         this.armInjuryHistory = "";
         this.priceLevel = "";
+        this.powerLevel = "";
     }
 
-    public Player(String exp, String arm, String price){
+    public Player(String exp, String arm, String price, String power){
         this.experienceLevel = exp;
         this.armInjuryHistory = arm;
         this.priceLevel = price;
+        this.powerLevel = power;
     }
 
     protected Player(Parcel in) {
         experienceLevel = in.readString();
         armInjuryHistory = in.readString();
         priceLevel = in.readString();
+        powerLevel = in.readString();
     }
 
     @Override
@@ -31,6 +35,7 @@ public class Player implements Parcelable {
         dest.writeString(experienceLevel);
         dest.writeString(armInjuryHistory);
         dest.writeString(priceLevel);
+        dest.writeString(powerLevel);
     }
 
     @Override
@@ -62,6 +67,10 @@ public class Player implements Parcelable {
         this.priceLevel = priceLevel;
     }
 
+    public void setPowerLevel(String powerLevel){
+        this.powerLevel = powerLevel;
+    }
+
     public String getExperienceLevel() {
         return experienceLevel;
     }
@@ -74,6 +83,8 @@ public class Player implements Parcelable {
         return priceLevel;
     }
 
+    public String getPowerLevel() { return powerLevel; }
+
     public String recommend(){
         String recommendation = "";
         switch(this.experienceLevel){
@@ -82,26 +93,81 @@ public class Player implements Parcelable {
                     case "No":
                         switch(this.priceLevel){
                             case "Low":
-                                recommendation = "Wilson Synthetic Gut 16 at 55 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Wilson Synthetic Gut 16 at 58 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson Synthetic Gut 16 at 55 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson Synthetic Gut 16 at 52 lbs";
+                                        return recommendation;
+                                }
                             case "Medium":
-                                recommendation = "Wilson Sensation 16 at 55 lbs";
-                                return recommendation;
+                                switch(this.powerLevel) {
+                                    case "Low":
+                                        recommendation = "Wilson Sensation 16 at 58 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson Sensation 16 at 55 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson Sensation 16 at 52 lbs";
+                                        return recommendation;
+                                }
+
                             case "High":
-                                recommendation = "Wilson NXT 16 at 55 lbs";
-                                return recommendation;
+                                switch(this.powerLevel) {
+                                    case "Low":
+                                        recommendation = "Wilson NXT 16 at 58 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson NXT 16 at 55 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson NXT 16 at 52 lbs";
+                                        return recommendation;
+                                }
                         }
                     case "Yes":
                         switch(this.priceLevel){
                             case "Low":
-                                recommendation = "Wilson Synthetic Gut 16 at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Wilson Synthetic Gut 16 at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson Synthetic Gut 16 at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson Synthetic Gut 16 at 49 lbs";
+                                        return recommendation;
+                                }
                             case "Medium":
-                                recommendation = "Wilson Sensation 16 at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel) {
+                                    case "Low":
+                                        recommendation = "Wilson Sensation 16 at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson Sensation 16 at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson Sensation 16 at 49 lbs";
+                                        return recommendation;
+                                }
                             case "High":
-                                recommendation = "Wilson NXT 16 at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel) {
+                                    case "Low":
+                                        recommendation = "Wilson NXT 16 at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson NXT 16 at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson NXT 16 at 49 lbs";
+                                        return recommendation;
+                                }
                         }
 
 
@@ -111,26 +177,80 @@ public class Player implements Parcelable {
                     case "No":
                         switch(this.priceLevel){
                             case "Low":
-                                recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 54 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 57 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 54 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 51 lbs";
+                                        return recommendation;
+                                }
                             case "Medium":
-                                recommendation = "Solinco Tour Bite 16 mains and Wilson NXT 16 crosses at 54 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Solinco Tour Bite 16 mains and Wilson NXT 16 crosses at 57 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Solinco Tour Bite 16 mains and Wilson NXT 16 crosses at 54 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Solinco Tour Bite 16 mains and Wilson NXT 16 crosses at 51 lbs";
+                                        return recommendation;
+                                }
                             case "High":
-                                recommendation = "Luxilon ALU Power 16 mains and Wilson NXT 16 crosses at 54 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon ALU Power 16 mains and Wilson NXT 16 crosses at 57 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon ALU Power 16 mains and Wilson NXT 16 crosses at 54 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon ALU Power 16 mains and Wilson NXT 16 crosses at 51 lbs";
+                                        return recommendation;
+                                }
                         }
                     case "Yes":
                         switch(this.priceLevel){
                             case "Low":
-                                recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 50 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 53 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 50 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Synthetic Gut 16 crosses at 47 lbs";
+                                        return recommendation;
+                                }
                             case "Medium":
-                                recommendation = "Luxilon Adrenaline 16 mains and Wilson Sensation 16 crosses at 50 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Sensation 16 crosses at 53 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Sensation 16 crosses at 50 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon Adrenaline 16 mains and Wilson Sensation 16 crosses at 47 lbs";
+                                        return recommendation;
+                                }
                             case "High":
-                                recommendation = "Wilson ALU Power 16 mains and Wilson NXT crosses at 50 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Wilson ALU Power 16 mains and Wilson NXT crosses at 53 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson ALU Power 16 mains and Wilson NXT crosses at 50 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson ALU Power 16 mains and Wilson NXT crosses at 47 lbs";
+                                        return recommendation;
+                                }
                         }
 
 
@@ -140,26 +260,82 @@ public class Player implements Parcelable {
                     case "No":
                         switch(this.priceLevel){
                             case "Low":
-                                recommendation = "Luxilon Adrenaline Power 16 at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon Adrenaline Power 16 at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon Adrenaline Power 16 at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon Adrenaline Power 16 at 49 lbs";
+                                        return recommendation;
+                                }
                             case "Medium":
-                                recommendation = "Solinco Tour Bite 16 at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Solinco Tour Bite 16 at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Solinco Tour Bite 16 at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Solinco Tour Bite 16 at 49 lbs";
+                                        return recommendation;
+                                }
                             case "High":
-                                recommendation = "Luxilon ALU Power 16 at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon ALU Power 16 at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon ALU Power 16 at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon ALU Power 16 at 49 lbs";
+                                        return recommendation;
+                                }
                         }
                     case "Yes":
                         switch(this.priceLevel){
                             case "Low":
-                                recommendation = "Luxilon Adrenaline mains and Wilson Sensation 16 crosses at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Luxilon Adrenaline mains and Wilson Sensation 16 crosses at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Luxilon Adrenaline mains and Wilson Sensation 16 crosses at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Luxilon Adrenaline mains and Wilson Sensation 16 crosses at 49 lbs";
+                                        return recommendation;
+                                }
+
                             case "Medium":
-                                recommendation = "Solinco Tour Bite 16 mains and Wilson Sensation 16 crosses at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Solinco Tour Bite 16 mains and Wilson Sensation 16 crosses at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Solinco Tour Bite 16 mains and Wilson Sensation 16 crosses at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Solinco Tour Bite 16 mains and Wilson Sensation 16 crosses at 49 lbs";
+                                        return recommendation;
+                                }
                             case "High":
-                                recommendation = "Wilson Natural Gut 16 mains and Luxilon ALU Power crosses at 52 lbs";
-                                return recommendation;
+                                switch(this.powerLevel){
+                                    case "Low":
+                                        recommendation = "Wilson Natural Gut 16 mains and Luxilon ALU Power crosses at 55 lbs";
+                                        return recommendation;
+                                    case "Medium":
+                                        recommendation = "Wilson Natural Gut 16 mains and Luxilon ALU Power crosses at 52 lbs";
+                                        return recommendation;
+                                    case "High":
+                                        recommendation = "Wilson Natural Gut 16 mains and Luxilon ALU Power crosses at 49 lbs";
+                                        return recommendation;
+                                }
+
                         }
 
 
--- app/src/main/java/com/example/tennisstringrecommender/PowerLevel.java
+++ app/src/main/java/com/example/tennisstringrecommender/PowerLevel.java
@@ -2,13 +2,46 @@ package com.example.tennisstringrecommender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class PowerLevel extends AppCompatActivity {
+
+    Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_level);
+
+        Intent intent = getIntent();
+        player = intent.getParcelableExtra("radioChosen");
+    }
+
+    public void onRadioPowerClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.powerLow:
+                if (checked)
+                    player.setPowerLevel("Low");
+                break;
+            case R.id.powerMedium:
+                if (checked)
+                    player.setPowerLevel("Medium");
+                break;
+            case R.id.powerHigh:
+                if (checked)
+                    player.setPowerLevel("High");
+                break;
+        }
+
+        Intent intent = new Intent(this, Recommend.class);
+        intent.putExtra("radioChosen", player);
+        startActivity(intent);
+
     }
 }
